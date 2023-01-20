e6451830512d948623b8bf6359287fa9e01a1dbf
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-01-20 16:53:14 +0100

    Quatrieme

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2; // On n'oublie pas le `.v2` à la fin
-
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/andromeda-vinted");
 
+app.listen(process.env.PORT, () => {
+  console.log("Server started");
+});
+
 cloudinary.config({
-  cloud_name: "dw6rfhtj7",
-  api_key: "363994899767859",
-  api_secret: "UN_eq9HggTbqfrw6BLMwHI-S5Ls",
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
   secure: true,
 });
 
@@ -24,6 +28,6 @@ app.all("*", (req, res) => {
   res.status(404).json({ message: "This routes doesn't exist" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started");
 });
