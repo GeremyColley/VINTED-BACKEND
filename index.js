92bd2d0f8acf3f36b3f8a209af5e001a6faa0a51
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-02-13 17:53:05 +0100

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2; // On n'oublie pas le `.v2` à la fin
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 require('dotenv').config();
 
